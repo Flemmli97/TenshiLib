790ac81c22de7bd32cd1ed6a89e10e18fbc81ea7
Flemmli97 <Flemmli97@users.noreply.github.com>
2020-12-23 22:06:53 +0100

    support for non minecraft namespace autocomplete?

---
+++ src/main/java/com/flemmli97/tenshilib/mixin/ISuggestionProviderMixin.java
@@ -0,0 +1,24 @@
+package com.flemmli97.tenshilib.mixin;
+
+import net.minecraft.command.ISuggestionProvider;
+import net.minecraft.util.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+@Mixin(ISuggestionProvider.class)
+public abstract class ISuggestionProviderMixin {
+
+    @Inject(method = "func_210512_a", at = @At(value = "INVOKE"), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
+    static <T> void autocomplete(Iterable<T> registry, String str, Function<T, ResourceLocation> func, Consumer<T> cons, CallbackInfoReturnable<T> info, boolean flag, T t, ResourceLocation resourceLocation) {
+        if (ISuggestionProvider.method_27136(str, resourceLocation.getPath())) {
+            cons.accept(t);
+            info.cancel();
+        }
+    }
+}
