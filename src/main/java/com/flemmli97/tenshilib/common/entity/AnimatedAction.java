b100d0fda2546ff1d5a46d60b073b405b85acd79
Flemmli97 <Flemmli97@users.noreply.github.com>
2021-05-01 21:09:06 +0200

    add option to change the client animation reference

--- src/main/java/com/flemmli97/tenshilib/common/entity/AnimatedAction.java
+++ src/main/java/com/flemmli97/tenshilib/common/entity/AnimatedAction.java
@@ -6,23 +6,39 @@ public class AnimatedAction {
     public static final AnimatedAction[] vanillaAttackOnly = new AnimatedAction[]{vanillaAttack};
 
     private final int length, attackTime;
-    private final String id;
+    private final String id, animationClient;
     private float ticker;
     private float speed = 1;
 
     public AnimatedAction(int length, int attackTime, String id) {
+        this(length, attackTime, id, id);
+    }
+
+    /**
+     * @param length          Length of the animation
+     * @param attackTime      A flag for various things e.g. when the entity should actually do damage
+     *                        Example in a sword slash do the damage mid swing and not at the beginning.
+     * @param id              Unique id for the animation
+     * @param animationClient The animation to play on the client side. Normally same as id but for cases
+     *                        where you have multiple attacks with same animation set this
+     */
+    public AnimatedAction(int length, int attackTime, String id, String animationClient) {
         this.length = length;
         this.attackTime = attackTime;
         this.id = id;
+        this.animationClient = animationClient;
     }
 
-    public AnimatedAction(int length, int attackTime, String id, boolean loops, float speedMod) {
-        this(length, attackTime, id);
+    public AnimatedAction(int length, int attackTime, String id, String animationClient, float speedMod) {
+        this(length, attackTime, id, animationClient);
         this.speed = speedMod;
     }
 
+    /**
+     * @return Creates a new copy instance of the animation
+     */
     public AnimatedAction create() {
-        return new AnimatedAction(this.length, this.attackTime, this.id);
+        return new AnimatedAction(this.length, this.attackTime, this.id, this.animationClient, this.speed);
     }
 
     public boolean tick() {
@@ -55,6 +71,10 @@ public class AnimatedAction {
 
     public boolean checkID(AnimatedAction other) {
         return other != null && this.id.equals(other.id);
+    }
+
+    public String getAnimationClient() {
+        return this.animationClient;
     }
 
     @Override
