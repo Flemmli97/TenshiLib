e22f8c72fd41857133c46ae08ee4d9b56af11deb
Flemmli97 <Flemmli97@users.noreply.github.com>
2022-03-03 18:45:47 +0100

    fix mixin failure on fabric

--- fabric/src/main/java/io/github/flemmli97/tenshilib/fabric/mixin/MinecraftMixin.java
+++ fabric/src/main/java/io/github/flemmli97/tenshilib/fabric/mixin/MinecraftMixin.java
@@ -5,14 +5,16 @@ import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
 
     @Inject(method = "startAttack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/phys/HitResult;getType()Lnet/minecraft/world/phys/HitResult$Type;", shift = At.Shift.BY, by = -3), cancellable = true)
-    private void leftClickAttack(CallbackInfo info) {
-        if (ClientEvents.clickSpecial())
+    private void leftClickAttack(CallbackInfoReturnable<Boolean> info) {
+        if (ClientEvents.clickSpecial()) {
+            info.setReturnValue(false);
             info.cancel();
+        }
     }
 }
