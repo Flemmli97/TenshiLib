c74f1082bdd0f434904b263fc2cdf8e099a4549a
Flemmli97 <Flemmli97@users.noreply.github.com>
2022-03-05 17:15:19 +0100

    fix IForgeRegistry mixin problems

---
+++ common/src/main/java/io/github/flemmli97/tenshilib/platform/registry/RegEntryInternal.java
@@ -0,0 +1,35 @@
+package io.github.flemmli97.tenshilib.platform.registry;
+
+import com.google.common.reflect.TypeToken;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Internal class. Use {@link CustomRegistryEntry} that has a more fitting generic constraint.
+ * Here for generic shenanigans when mixin IForgeRegistryEntry into this class
+ */
+class RegEntryInternal<V> {
+
+    @SuppressWarnings("UnstableApiUsage")
+    private final TypeToken<V> token = new TypeToken<>(this.getClass()) {
+    };
+    private ResourceLocation registryName = null;
+
+    @SuppressWarnings("unchecked")
+    public V setRegistryName(ResourceLocation res) {
+        if (this.getRegistryName() != null) {
+            throw new IllegalStateException("Attempted to set registry name with existing registry name! New: " + res + " Old: " + this.getRegistryName());
+        }
+        this.registryName = res;
+        return (V) this;
+    }
+
+    public @Nullable ResourceLocation getRegistryName() {
+        return this.registryName;
+    }
+
+    @SuppressWarnings({"unchecked", "UnstableApiUsage"})
+    public Class<V> getRegistryType() {
+        return (Class<V>) this.token.getRawType();
+    }
+}
--- forge/src/main/java/io/github/flemmli97/tenshilib/forge/mixin/ForgeRegEntryAdder.java
+++ forge/src/main/java/io/github/flemmli97/tenshilib/forge/mixin/ForgeRegEntryAdder.java
@@ -1,9 +1,9 @@
 package io.github.flemmli97.tenshilib.forge.mixin;
 
-import io.github.flemmli97.tenshilib.platform.registry.CustomRegistryEntry;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(CustomRegistryEntry.class)
-public abstract class ForgeRegEntryAdder<V extends CustomRegistryEntry<V>> implements IForgeRegistryEntry<V> {
+@Mixin(targets = "io/github/flemmli97/tenshilib/platform/registry/RegEntryInternal")
+public abstract class ForgeRegEntryAdder<V> implements IForgeRegistryEntry<V> {
+
 }
