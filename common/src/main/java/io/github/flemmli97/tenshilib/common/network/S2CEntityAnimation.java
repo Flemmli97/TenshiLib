4e2b16a5f4b587a51f1b4707706c73919826b7d9
Flemmli97 <Flemmli97@users.noreply.github.com>
2022-10-10 17:28:25 +0200

    some checks and helper

--- common/src/main/java/io/github/flemmli97/tenshilib/common/network/S2CEntityAnimation.java
+++ common/src/main/java/io/github/flemmli97/tenshilib/common/network/S2CEntityAnimation.java
@@ -8,6 +8,7 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 public class S2CEntityAnimation implements Packet {
@@ -40,6 +41,7 @@ public class S2CEntityAnimation implements Packet {
                                 break;
                             i++;
                         }
+                        TenshiLib.logger.error("This animation is not registered for {}. Registered animations are {} but set animation is {}", e, Arrays.toString(entity.getAnimationHandler().getAnimations()), anim);
                         if (i < entity.getAnimationHandler().getAnimations().length)
                             return i;
                         return -2;
