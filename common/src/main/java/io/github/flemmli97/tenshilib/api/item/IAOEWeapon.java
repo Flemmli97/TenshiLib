a8e1f4c9653abac332b277201a7e17a02061ecde
Flemmli97 <Flemmli97@users.noreply.github.com>
2023-08-07 22:40:40 +0200

    stack sensitive aoe and ext weapons

--- common/src/main/java/io/github/flemmli97/tenshilib/api/item/IAOEWeapon.java
+++ common/src/main/java/io/github/flemmli97/tenshilib/api/item/IAOEWeapon.java
@@ -1,16 +1,23 @@
 package io.github.flemmli97.tenshilib.api.item;
 
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+
 /**
  * Items with modified attack range and aoe.
  */
 public interface IAOEWeapon {
 
-    float getRange();
+    default float getRange(LivingEntity entity, ItemStack stack) {
+        return 3;
+    }
 
     /**
      * @return FOV of the weapons aoe area in degree. E.g. getFOV()==10 would mean all entities at the crosshair position +- 10 degrees left and right. Max 180
      */
-    float getFOV();
+    default float getFOV(LivingEntity entity, ItemStack stack) {
+        return 0;
+    }
 
     default boolean doSweepingAttack() {
         return true;
--- common/src/main/java/io/github/flemmli97/tenshilib/api/item/IExtendedWeapon.java
+++ common/src/main/java/io/github/flemmli97/tenshilib/api/item/IExtendedWeapon.java
@@ -1,10 +1,14 @@
 package io.github.flemmli97.tenshilib.api.item;
 
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+
 /**
  * Items with modified attack range
  */
 public interface IExtendedWeapon {
 
-    float getRange();
-
+    default float getRange(LivingEntity entity, ItemStack stack) {
+        return 3;
+    }
 }
