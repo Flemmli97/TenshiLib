18402a19c56afa7574a2e3f444f9ccaa56d5a255
Flemmli97 <Flemmli97@users.noreply.github.com>
2023-08-07 22:30:39 +0200

    rendering changes/fixes from 1.18 branch

---
+++ common/src/main/java/io/github/flemmli97/tenshilib/client/render/RenderCrossedTextureEntity.java
@@ -0,0 +1,38 @@
+package io.github.flemmli97.tenshilib.client.render;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.world.entity.Entity;
+
+/**
+ * Render an entity using a texture in the shape of an arrow entity
+ */
+public abstract class RenderCrossedTextureEntity<T extends Entity> extends RenderTexture<T> {
+
+    public RenderCrossedTextureEntity(EntityRendererProvider.Context ctx, float xSize, float ySize, int rows, int columns) {
+        super(ctx, xSize, ySize, rows, columns);
+    }
+
+    @Override
+    public void doRender(T entity, float partialTicks, PoseStack stack, MultiBufferSource buffer) {
+        stack.pushPose();
+        stack.mulPose(Axis.XP.rotationDegrees(45.0F));
+        for (int j = 0; j < 2; ++j) {
+            stack.mulPose(Axis.XP.rotationDegrees(90.0F));
+            RenderUtils.renderTexture(stack, buffer.getBuffer(this.getRenderType(entity, this.getTextureLocation(entity))), this.xSize, this.ySize, this.textureBuilder);
+        }
+        stack.popPose();
+    }
+
+    @Override
+    public boolean facePlayer() {
+        return false;
+    }
+
+    @Override
+    public float yawOffset() {
+        return -90.0F;
+    }
+}
