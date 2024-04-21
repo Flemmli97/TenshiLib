9dd8dc30d583e0827a477e3d58e18192d1c7774b
Flemmli97 <Flemmli97@users.noreply.github.com>
2024-04-21 19:14:46 +0200

    Send animation data on tracking

--- forge/src/main/java/io/github/flemmli97/tenshilib/forge/events/CommonEvents.java
+++ forge/src/main/java/io/github/flemmli97/tenshilib/forge/events/CommonEvents.java
@@ -1,7 +1,13 @@
 package io.github.flemmli97.tenshilib.forge.events;
 
+import io.github.flemmli97.tenshilib.api.entity.IAnimated;
 import io.github.flemmli97.tenshilib.api.item.IDualWeapon;
+import io.github.flemmli97.tenshilib.common.network.S2CEntityAnimation;
+import io.github.flemmli97.tenshilib.forge.network.PacketHandler;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.Event;
 
@@ -15,6 +21,12 @@ public class CommonEvents {
     public static void disableOffhandBlock(PlayerInteractEvent.RightClickBlock event) {
         if (event.getHand() == InteractionHand.OFF_HAND && event.getPlayer().getMainHandItem().getItem() instanceof IDualWeapon weapon && weapon.disableOffhand()) {
             event.setUseItem(Event.Result.DENY);
+        }
+    }
+
+    public static void onTracking(PlayerEvent.StartTracking event) {
+        if (event.getTarget() instanceof IAnimated animated && animated.getAnimationHandler().hasAnimation()) {
+            PacketHandler.sendToClientChecked(S2CEntityAnimation.create((Entity & IAnimated) event.getTarget()), (ServerPlayer) event.getPlayer());
         }
     }
 }
