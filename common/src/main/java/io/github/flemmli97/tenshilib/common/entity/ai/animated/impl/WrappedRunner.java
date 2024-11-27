63229fefd616ca6b063c116349102fe5c7a605be
Flemmli97 <Flemmli97@users.noreply.github.com>
2024-11-28 00:23:32 +0100

    decrease default timeout for ai runners

--- common/src/main/java/io/github/flemmli97/tenshilib/common/entity/ai/animated/impl/WrappedRunner.java
+++ common/src/main/java/io/github/flemmli97/tenshilib/common/entity/ai/animated/impl/WrappedRunner.java
@@ -19,7 +19,7 @@ public class WrappedRunner<T extends PathfinderMob & IAnimated> implements Actio
     private final ActionRun<T> runner;
 
     public WrappedRunner(ActionRun<T> runner) {
-        this(e -> 100, runner);
+        this(e -> 80, runner);
     }
 
     public WrappedRunner(GoalAttackAction.IntProvider<T> timout, ActionRun<T> runner) {
