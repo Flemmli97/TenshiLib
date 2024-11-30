8b6e3fad67dce6b7e5755b7d49653c8c1104e351
Flemmli97 <Flemmli97@users.noreply.github.com>
2024-11-30 14:03:22 +0100

    fix packet not optional

--- neoforge/src/main/java/io/github/flemmli97/tenshilib/forge/network/PacketHandler.java
+++ neoforge/src/main/java/io/github/flemmli97/tenshilib/forge/network/PacketHandler.java
@@ -17,7 +17,7 @@ import java.util.function.BiConsumer;
 public class PacketHandler {
 
     public static void register(RegisterPayloadHandlersEvent event) {
-        PayloadRegistrar registrar = event.registrar(TenshiLib.MODID);
+        PayloadRegistrar registrar = event.registrar(TenshiLib.MODID).optional();
         PacketRegistrar.registerServerPackets(new PacketRegistrar.ServerPacketRegister() {
             @Override
             public <P extends CustomPacketPayload> void register(CustomPacketPayload.Type<P> type, StreamCodec<RegistryFriendlyByteBuf, P> codec, BiConsumer<P, ServerPlayer> handler) {
