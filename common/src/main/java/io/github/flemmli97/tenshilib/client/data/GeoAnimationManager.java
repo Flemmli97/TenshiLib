949deba60c0a420061dd6d21fd4d1e09bbbd1ed3
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-10-13 22:16:52 +0200

    fix optional animation

--- common/src/main/java/io/github/flemmli97/tenshilib/client/data/GeoAnimationManager.java
+++ common/src/main/java/io/github/flemmli97/tenshilib/client/data/GeoAnimationManager.java
@@ -7,6 +7,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,6 +15,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Consumer;
 
 /**
  * Stores BedrockAnimations.
@@ -72,16 +74,27 @@ public class GeoAnimationManager extends SimpleJsonResourceReloadListener {
 
     public ReloadableCache<BedrockAnimations> getOptionalAnimation(ResourceLocation id) {
         this.optional.add(id);
-        ReloadableCache<BedrockAnimations> cache = this.getAnimation(id);
-        cache.update(BedrockAnimations.empty());
-        return cache;
+        return this.tryGetAnimation(id, cache -> cache.update(BedrockAnimations.empty()));
     }
 
     public ReloadableCache<BedrockAnimations> getAnimation(ResourceLocation id) {
+        return this.tryGetAnimation(id, null);
+    }
+
+    private ReloadableCache<BedrockAnimations> tryGetAnimation(ResourceLocation id, @Nullable Consumer<ReloadableCache<BedrockAnimations>> init) {
         return this.animations.computeIfAbsent(id, r -> {
-            if (this.reloaded)
-                TenshiLib.LOGGER.error("Animation {} is not present! Returned result will be empty!", r);
-            return new ReloadableCache<>();
+            ReloadableCache<BedrockAnimations> cache = new ReloadableCache<>();
+            if (init != null) {
+                init.accept(cache);
+            }
+            if (this.reloaded) {
+                if (cache.get() == null) {
+                    throw new IllegalStateException("Animation is not present! " + r);
+                } else {
+                    TenshiLib.LOGGER.error("Animation {} is not present! Returned result will be empty!", r);
+                }
+            }
+            return cache;
         });
     }
 }
