24e09dc7812b6583d527642a624b999823a857cf
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-10-27 01:40:46 +0100

    make attachment easier to fetch

--- common/src/main/java/io/github/flemmli97/tenshilib/common/attachment/AttachmentType.java
+++ common/src/main/java/io/github/flemmli97/tenshilib/common/attachment/AttachmentType.java
@@ -1,5 +1,8 @@
 package io.github.flemmli97.tenshilib.common.attachment;
 
+import io.github.flemmli97.tenshilib.loader.registry.AttachmentRegister;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
@@ -38,6 +41,38 @@ public class AttachmentType<T> {
 
     public Optional<AttachmentTransferHandler<T>> transferHandler() {
         return Optional.ofNullable(this.transferHandler);
+    }
+
+    /**
+     * Shortcut to platform implementations.
+     * Gets the current attachment on the given holder. Creates a new one if it doesn't exist
+     * <p>
+     * Only {@link Entity} and {@link BlockEntity} supported!
+     */
+    public T get(Object holder) {
+        if (holder instanceof Entity entity) {
+            return AttachmentRegister.INSTANCE.getAttachment(entity, this);
+        }
+        if (holder instanceof BlockEntity blockEntity) {
+            return AttachmentRegister.INSTANCE.getAttachment(blockEntity, this);
+        }
+        throw new IllegalStateException("Unsupported attachment holder");
+    }
+
+    /**
+     * Shortcut to platform implementations.
+     * Gets the current attachment on the given holder if exists
+     * <p>
+     * Only {@link Entity} and {@link BlockEntity} supported!
+     */
+    public Optional<T> getOptional(Object holder) {
+        if (holder instanceof Entity entity) {
+            return AttachmentRegister.INSTANCE.getOptionalAttachment(entity, this);
+        }
+        if (holder instanceof BlockEntity blockEntity) {
+            return AttachmentRegister.INSTANCE.getOptionalAttachment(blockEntity, this);
+        }
+        throw new IllegalStateException("Unsupported attachment holder");
     }
 
     public static class Builder<T> {
