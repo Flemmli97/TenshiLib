bd82724f6f5fc6dc70542c7e6ad5c98713f732d4
Flemmli97 <Flemmli97@users.noreply.github.com>
2026-01-06 13:05:47 +0100

    add ExtendedEntityModel that saves partialtick

---
+++ common/src/main/java/io/github/flemmli97/tenshilib/client/model/ExtendedEntityModel.java
@@ -0,0 +1,41 @@
+package io.github.flemmli97.tenshilib.client.model;
+
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.ref.WeakReference;
+import java.util.function.Function;
+
+public abstract class ExtendedEntityModel<T extends Entity> extends EntityModel<T> implements ExtendedModel {
+
+    protected float partialTick;
+
+    protected WeakReference<T> entity;
+
+    protected ExtendedEntityModel() {
+        super();
+    }
+
+    protected ExtendedEntityModel(Function<ResourceLocation, RenderType> renderType) {
+        super(renderType);
+    }
+
+    @Override
+    public void prepareMobModel(T entity, float limbSwing, float limbSwingAmount, float partialTick) {
+        this.entity = new WeakReference<>(entity);
+        this.partialTick = partialTick;
+    }
+
+    @Override
+    public float getPartialTick() {
+        return this.partialTick;
+    }
+
+    @Nullable
+    public T getCurrentEntity() {
+        return this.entity != null ? this.entity.get() : null;
+    }
+}
--- common/src/main/java/io/github/flemmli97/tenshilib/client/model/ExtendedModel.java
+++ common/src/main/java/io/github/flemmli97/tenshilib/client/model/ExtendedModel.java
@@ -2,5 +2,9 @@ package io.github.flemmli97.tenshilib.client.model;
 
 public interface ExtendedModel {
 
+    default float getPartialTick() {
+        return 1;
+    }
+
     ModelPartsContainer getModel();
 }
